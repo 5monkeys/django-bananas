a76d6befdb98b297b18e0849df4db0d18f37bfc0
Jonas Lundberg <jonas@5monkeys.se>
2018-12-05 10:52:47 +0100

    Change language on django 2 container

--- example/example/settings.py
+++ example/example/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from bananas.environment import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = env.get('DJANGO_LANGUAGE_CODE', 'en-us')
 
 TIME_ZONE = 'UTC'
 
