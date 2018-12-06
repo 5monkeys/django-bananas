f915d4ff08e9500414e92fd906a2b17af61522c0
Jonas Lundberg <jonas@5monkeys.se>
2018-12-06 15:55:46 +0100

    More example api

--- example/example/api.py
+++ example/example/api.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.response import Response
 
 from bananas.admin.api.views import BananasAPI
+from bananas.compat import reverse
 from bananas.lazy import lazy_title
 
 
@@ -16,12 +19,12 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         """
         This is needed due to DRF's model serializer uses the queryset to build url name
 
-        # TODO: Move this to own HyperlinkedModelSerializer
+        # TODO: Move this to own serializer mixin or fix problem elsewhere?
         """
         field, kwargs = super().build_url_field(field_name, model_class)
 
-        view = self.root.context['view']
-        kwargs['view_name'] = view.get_reverse_name('detail')
+        view = self.root.context["view"]
+        kwargs["view_name"] = view.get_url_name("detail")
 
         return field, kwargs
 
@@ -33,5 +36,14 @@ class UserViewSet(BananasAPI, viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    class Admin:
-        verbose_name = "User"
+    @action(detail=False)
+    def foo(self, request):  # , **kwargs):
+        return Response("Just a simple extra list action")
+
+    @action(detail=True)
+    def bar(self, request, pk):  # , **kwargs):
+        url = reverse(
+            "bananas:v1.0:example.user-bar",
+            kwargs={"version": request.version, "pk": pk},
+        )
+        return Response(f"Just a simple extra detail action, url = {url}")
