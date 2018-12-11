5f316dee2d54788087663d15c853b76d655a1a5b
Jonas Lundberg <jonas@5monkeys.se>
2018-12-11 17:14:43 +0100

    Remove router register kwarg to support older DRF version

--- bananas/admin/api/router.py
+++ bananas/admin/api/router.py
@@ -7,7 +7,7 @@ __all__ = ["register"]
 def register(view: BananasAPI):
     meta = view.get_admin_meta()
     prefix = meta.basename.replace(".", "/")
-    router.register(prefix, view, basename=meta.basename)
+    router.register(prefix, view, meta.basename)
 
 
 router = BananasRouter()
