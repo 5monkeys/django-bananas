25c3d624a23f8031f7025bf4c355cde1c56c5e34
Jonas Lundberg <jonas@5monkeys.se>
2018-12-12 12:24:14 +0100

    Complete tests for base admin api actions

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -54,6 +54,16 @@ class AdminBaseTest(TestCase):
         self.client.login(username=user.username, password="test")
         return user
 
+    def assertAuthorized(self):
+        url = compat.reverse("admin:index")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def assertNotAuthorized(self):
+        url = compat.reverse("admin:index")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)  # Redirect to login
+
 
 class AdminTest(AdminBaseTest):
     def setUp(self):
@@ -183,8 +193,94 @@ class AdminTest(AdminBaseTest):
 
 
 class APITest(AdminBaseTest):
-
-    def test_schema(self):
+    def test_unautorized_schema(self):
         url = compat.reverse("bananas:v1.0:schema", kwargs={"format": ".json"})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+        self.assertIn("/bananas/login/", data["paths"])
+        action = data["paths"]["/bananas/login/"]["post"]
+        self.assertEqual(action["operationId"], "bananas.login:create")
+        self.assertEqual(action["summary"], "Log in")
+        self.assertIn("navigation", action["tags"])
+
+    def test_autorized_schema(self):
+        self.login_user()
+        url = compat.reverse("bananas:v1.0:schema", kwargs={"format": ".json"})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+        self.assertNotIn("/bananas/login/", data["paths"])
+        self.assertIn("/bananas/logout/", data["paths"])
+        action = data["paths"]["/bananas/logout/"]["post"]
+        self.assertEqual(action["operationId"], "bananas.logout:create")
+        self.assertEqual(action["summary"], "Log out")
+        self.assertIn("navigation", action["tags"])
+
+    def test_login(self):
+        user = self.create_user()
+        url = compat.reverse("bananas:v1.0:bananas.login-list")
+
+        # Fail
+        response = self.client.post(
+            url, data={"username": user.username, "password": "fail"}
+        )
+        self.assertEqual(response.status_code, 400)
+
+        # Success
+        response = self.client.post(
+            url, data={"username": user.username, "password": "test"}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["username"], user.username)
+        self.assertAuthorized()
+
+    def test_logout(self):
+        self.login_user()
+        url = compat.reverse("bananas:v1.0:bananas.logout-list")
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 204)
+        self.assertNotAuthorized()
+
+    def test_change_password(self):
+        user = self.login_user()
+        url = compat.reverse("bananas:v1.0:bananas.change_password-list")
+
+        response = self.client.post(
+            url,
+            data={
+                "old_password": "foo",
+                "new_password1": "foo",
+                "new_password2": "foo",
+            },
+        )
+        self.assertEqual(response.status_code, 400)
+
+        response = self.client.post(
+            url,
+            data={
+                "old_password": "test",
+                "new_password1": "foo",
+                "new_password2": "bar",
+            },
+        )
+        self.assertEqual(response.status_code, 400)
+
+        response = self.client.post(
+            url,
+            data={
+                "old_password": "test",
+                "new_password1": "foobar123",
+                "new_password2": "foobar123",
+            },
+        )
+        self.assertEqual(response.status_code, 204)
+
+        self.client.logout()
+        self.client.login(username=user.username, password="test")
+        self.assertNotAuthorized()
+
+        self.client.login(username=user.username, password="foobar123")
+        self.assertAuthorized()
