10eb198e799f7d377b48b0cb77ed545d7da52540
Jonas Lundberg <jonas@5monkeys.se>
2019-01-22 10:04:42 +0100

    Fix type hint in router register

--- bananas/admin/api/router.py
+++ bananas/admin/api/router.py
@@ -1,10 +1,12 @@
+from typing import Type
+
 from .mixins import BananasAPI
 from .schemas import BananasRouter
 
 __all__ = ["register"]
 
 
-def register(view: BananasAPI):
+def register(view: Type[BananasAPI]):
     meta = view.get_admin_meta()
     prefix = meta.basename.replace(".", "/")
     router.register(prefix, view, meta.basename)
