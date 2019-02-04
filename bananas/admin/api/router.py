8d09e70b4b564d72a09224484bc8a01209a271c3
Jonas Lundberg <jonas@5monkeys.se>
2019-02-04 15:11:14 +0100

    Remove typing for py34

--- bananas/admin/api/router.py
+++ bananas/admin/api/router.py
@@ -1,12 +1,14 @@
-from typing import Type
-
-from .mixins import BananasAPI
 from .schemas import BananasRouter
 
 __all__ = ["register"]
 
 
-def register(view: Type[BananasAPI]):
+def register(view):  # Type[BananasAPI]
+    """
+    Register the API view class in the bananas router.
+
+    :param BananasAPI view:
+    """
     meta = view.get_admin_meta()
     prefix = meta.basename.replace(".", "/")
     router.register(prefix, view, meta.basename)
