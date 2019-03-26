433e2f261aaa9cdfac6a108c7d98ac4a4be6008b
Jonas Lundberg <jonas@5monkeys.se>
2019-03-26 15:14:18 +0100

    Expose all django messages in i18n api

--- bananas/admin/api/i18n.py
+++ bananas/admin/api/i18n.py
@@ -2,5 +2,8 @@ from django.views.i18n import JavaScriptCatalog
 
 
 class RawTranslationCatalog(JavaScriptCatalog):
+
+    domain = "django"
+
     def render_to_response(self, context, **response_kwargs):
         return context
