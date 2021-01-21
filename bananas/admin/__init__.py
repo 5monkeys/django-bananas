80caf6a8d05c42beaa1de1ce8e0dc9017a637b9f
Anton Agestam <git@antonagestam.se>
2021-01-21 17:25:44 +0100

    Make exports explicit

--- bananas/admin/__init__.py
+++ bananas/admin/__init__.py
@@ -1,1 +1,9 @@
-from .extension import *  # noqa
+from .extension import AdminView, ModelAdminView, ViewTool, register, site
+
+__all__ = (
+    "ModelAdminView",
+    "ViewTool",
+    "AdminView",
+    "register",
+    "site",
+)
--- bananas/admin/api/schemas/__init__.py
+++ bananas/admin/api/schemas/__init__.py
@@ -1,9 +1,16 @@
-from drf_yasg.utils import (  # noqa
+from drf_yasg.utils import (
     swagger_auto_schema as schema,
     swagger_serializer_method as schema_serializer_method,
 )
 
-from .yasg import (  # noqa
+from .yasg import (
     BananasSimpleRouter as BananasRouter,
     BananasSwaggerSchema as BananasSchema,
+)
+
+__all__ = (
+    "schema",
+    "schema_serializer_method",
+    "BananasRouter",
+    "BananasSchema",
 )
